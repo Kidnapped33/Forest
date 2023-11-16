3613b21c13582a25e5ad366174dbabf6d353f2e8
Ashine33 <ashine333@foxmail.com>
2023-11-16 21:54:07 +0800

    feat: 使用 htmlwebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('Html-webpack-plugin')
 
 module.exports = {
   mode: "production",
   entry: {
-    index: "./lib/index.tsx",
+    index: "./lib/index.tsx",   
   },
   output: {
     path: path.resolve(__dirname, "dist/lib"),
     library: "aui",
     libraryTarget: "umd",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin({
+    title:'forestUI',
+    // template:'index.html'
+  })],
   module: {
     rules: [
       {
