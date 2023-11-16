7c00532a6f84f72e2adb58a21e73efb0eb14e7dd
Ashine33 <ashine333@foxmail.com>
2023-11-17 00:24:36 +0800

    feat: 配置 webpack 的 resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('Html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("Html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: {
-    index: "./lib/index.tsx",   
+    index: "./lib/index.tsx",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"], // 指定可以省略的文件扩展名
+    // alias: {
+    //   src: path.resolve(__dirname, 'src'), // 设置别名，方便引用 src 目录下的模块
+    // },
   },
   output: {
     path: path.resolve(__dirname, "dist/lib"),
     library: "aui",
     libraryTarget: "umd",
   },
-  plugins: [new HtmlWebpackPlugin({
-    title:'forestUI',
-    // template:'index.html'
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "forestUI",
+      // template:'index.html'
+    }),
+  ],
   module: {
     rules: [
       {
