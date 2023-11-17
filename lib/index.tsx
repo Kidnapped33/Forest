efbce9f5c9e29b6353d3447a702eb640bb1759de
Ashine33 <ashine333@foxmail.com>
2023-11-17 10:57:15 +0800

    feat: react18 建议使用 createRoot

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+// import * as ReactDOM from 'react-dom'
 import Button from './button';
+import { createRoot } from 'react-dom/client';
 
 
-ReactDOM.render(<Button />, document.body)
+// ReactDOM.render(<Button />, document.body)
+
+
+const root = createRoot(document.body);
+// const root = document.createElement('root');
+root.render(<Button />);
+
+
 
